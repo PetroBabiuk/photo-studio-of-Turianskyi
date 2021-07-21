d53844837736fb14a07ae5d5430f331a5c9396e7
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-07-21 09:45:04 +0300

    добавляет хедер

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+import headerTemplate from './templates/header.hbs';
+
+const refs = {
+    body: document.querySelector('body'),
+}
+
+function addHeader() {
+refs.body.insertAdjacentHTML('afterbegin', headerTemplate());
+}
+
+addHeader();
